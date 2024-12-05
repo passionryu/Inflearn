790c3d324ff922388552456ee810cc0f1546f847
passionryu <rsy1225@naver.com>
2024-12-05 19:13:50 +0900

    chore : 테스팅 용 주석

--- 스프링핵심원리_기본편/core/src/main/java/hello/core/Member/MemberServiceImpl.java
+++ 스프링핵심원리_기본편/core/src/main/java/hello/core/Member/MemberServiceImpl.java
@@ -4,11 +4,20 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemberMemberRepository();
 
+    /**
+     * 회원 가입 메서드
+     * @param member
+     */
     @Override
     public void join(Member member) {
         memberRepository.save(member);
     }
 
+    /**
+     * 아이디로 멤버 조회 메서드
+     * @param memberId
+     * @return
+     */
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
