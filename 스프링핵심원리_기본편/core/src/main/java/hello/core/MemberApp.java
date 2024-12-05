e0f1921fd01015a65f84e4ed10226045fa75ad73
SeongYeol_Ryu <141282425+passionryu@users.noreply.github.com>
2024-12-05 18:53:54 +0900

    Add files via upload

---
+++ 스프링핵심원리_기본편/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.Member.Grade;
+import hello.core.Member.Member;
+import hello.core.Member.MemberService;
+import hello.core.Member.MemberServiceImpl;
+
+public class MemberApp {
+    /**
+     * spring 프레임 워크를 사용하지 않고, 순수하게 자바 코드로만 실행
+     * @param args
+     */
+    public static void main(String[] args){
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L,"memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
