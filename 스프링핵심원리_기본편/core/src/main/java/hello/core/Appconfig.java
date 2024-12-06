5028127070a4d8de0431e02b8cfe2c4cc21e0b72
passionryu <rsy1225@naver.com>
2024-12-06 20:13:45 +0900

    add : 4.4 appconfig 리펙토링

--- 스프링핵심원리_기본편/core/src/main/java/hello/core/Appconfig.java
+++ 스프링핵심원리_기본편/core/src/main/java/hello/core/Appconfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.Discount.DiscountPolicy;
 import hello.core.Discount.FixDiscountPolicy;
+import hello.core.Member.MemberRepository;
 import hello.core.Member.MemberService;
 import hello.core.Member.MemberServiceImpl;
 import hello.core.Member.MemoryMemberRepository;
@@ -9,14 +11,24 @@ import hello.core.Order.OrderServiceImpl;
 
 public class Appconfig {
 
+    /* 리펙토링 시  컨트롤 + 알트 + M 을 누르면 된다.*/
+
     // 멤버 서비스가 메모리 멤버 레포지토리를 주입받는다.
     // 생성자 주입
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+    private static MemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-            return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+            return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
+    }
+
+    private static DiscountPolicy DiscountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
